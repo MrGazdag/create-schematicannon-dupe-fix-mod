edb1014e2f547a3bffe07c2b2120b7c859c976bd
MrGazdag <44264503+MrGazdag@users.noreply.github.com>
2026-01-26 15:46:01 +0100

    Initial commit

---
+++ src/main/java/dev/mrgazdag/createschematicannondupefix/CreateSchematicannonDupeFixMod.java
@@ -0,0 +1,16 @@
+package dev.mrgazdag.createschematicannondupefix;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CreateSchematicannonDupeFixMod.MODID)
+public class CreateSchematicannonDupeFixMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "create_schematicannon_dupe_fix";
+
+    public CreateSchematicannonDupeFixMod(FMLJavaModLoadingContext context)
+    {
+    }
+}
